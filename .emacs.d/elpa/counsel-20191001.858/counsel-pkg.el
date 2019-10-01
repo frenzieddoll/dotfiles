e559d30dc4bddbf8f89846d285f62a891204ca07
frenzieddoll <frenzieddoll@gmail.com>
2019-10-02 01:41:38 +0900

    20191001

---
+++ .emacs.d/elpa/counsel-20191001.858/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/toshiaki/.emacs.d/elpa/counsel-20191001.858/counsel.el  -*- no-byte-compile: t -*-
+(define-package "counsel" "20191001.858" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.12.0")) :commit "464875a58a113db32c1902fb6cab8db015b95542" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
