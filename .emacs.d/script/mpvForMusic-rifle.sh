7777312cfc4eb21bf82e8791b9e8a2942244be96
frenzieddoll <frenzieddoll@gmail.com>
2020-02-03 02:15:21 +0900

    20200203

---
+++ .emacs.d/script/mpvForMusic-rifle.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: ${0##*/} PICTURES"
+    exit
+fi
+
+[ "$1" = '--' ] && shift
+
+abspath () {
+    case "$1" in
+        /*) printf "%s\n" "$1";;
+        ./*) printf "%s\n" "$(basename "$1")";;
+        *)  printf "%s\n" "$PWD/$1";;
+    esac
+}
+
+extentions=".*\(mp3\)$"
+
+target="$(abspath "$1")"
+find "$(dirname "$target")" -maxdepth 1 -type f -iregex $extentions | sort > playlist
+count="$(grep -n "$1" playlist | cut -d ":" -f 1)"
+
+if [ "$(wc -l playlist)" == 1 ]; then
+    mpv $target
+elif [ -n "$count" ]; then
+    mpv --playlist=playlist --playlist-start=$(($count - 1))
+else
+    echo "error"
+fi
+
+rm playlist
