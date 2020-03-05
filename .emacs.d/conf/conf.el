37424801d01189be66b284606c64436615093cbb
frenzieddoll <frenzieddoll@gmail.com>
2020-03-05 15:34:11 +0900

    20200305

--- .emacs.d/conf/conf.el
+++ .emacs.d/conf/conf.el
@@ -1365,47 +1365,61 @@
 
 
 ;; 途中の設定
-(leaf mew
-  :disabled t
-  :when (file-exists-p '"/usr/bin/stunell")
-  :ensure t
-  :defvar mew-fcc mew-smtp-server mew-smtp-auth
-  :custom ((exec-path . '(cons "/usr/bin" exec-path))
-  		   (user-mail-address . "frenzieddoll@gmail.com")
-  		   (user-full-name . "frenzieddoll")
-  		   (mew-smtp-server . "smtp.gmail.com")
-  		   (mail-user-agent . 'mew-user-agent)
-  		   ;; Stunnel
-  		   (mew-prog-ssl . "/usr/bin/stunnel")
-  		   ;; IMAP for Gmail
-  		   (mew-proto . "%")
-  		   (mew-imap-server . "imap.gmail.com")
-  		   (mew-imap-user . "frenzieddoll@gmail.com")
-  		   (mew-imap-auth .  t)
-  		   (mew-imap-ssl . t)
-  		   (mew-imap-ssl-port . "993")
-  		   (mew-smtp-auth . t)
-  		   (mew-smtp-ssl . t)
-  		   (mew-smtp-ssl-port . "465")
-  		   (mew-smtp-user . "frenzieddoll@gmail.com")
-  		   (mew-smtp-server . "smtp.gmail.com")
-  		   (mew-fcc . "%Sent")
-  		   (mew-imap-trash-folder . "%[Gmail]/Trash")
-  		   (mew-use-cached-passwd . t)
-  		   (mew-ssl-verify-level . 0)
 
-  		   (mew-use-cached-passwd . t)
-  		   (mew-use-master-passwd . t)
-  		   ;; 未読メールにUマークを付ける
-  		   (mew-use-unread-mark . t))
-  :config
-  (define-mail-user-agent
-	'mew-user-agent
-	'mew-user-agent-compose
-	'mew-draft-send-message
-	'mew-draft-kill
-	'mew-send-hook)
-  )
+(leaf mew
+	;; :disabled t
+	:when (file-exists-p '"/usr/bin/stunnel")
+	:ensure t
+	:require t
+	:defvar mew-fcc mew-smtp-server mew-smtp-auth
+	:custom ((mew-fcc . "+outbox")
+			 (exec-path . '(cons "/usr/bin" exec-path))
+  			 (user-mail-address . "frenzieddoll@gmail.com")
+  			 (user-full-name . "frenzieddoll")
+  			 (mew-smtp-server . "smtp.gmail.com")
+  			 (mail-user-agent . 'mew-user-agent)
+  			 ;; Stunnel
+  			 (mew-prog-ssl . "/usr/bin/stunnel")
+  			 ;; IMAP for Gmail
+  			 (mew-proto . "%")
+  			 (mew-imap-server . "imap.gmail.com")
+  			 (mew-imap-user . "frenzieddoll@gmail.com")
+  			 (mew-imap-auth .  t)
+  			 (mew-imap-ssl . t)
+  			 (mew-imap-ssl-port . "993")
+  			 (mew-smtp-auth . t)
+  			 (mew-smtp-ssl . t)
+  			 (mew-smtp-ssl-port . "465")
+  			 (mew-smtp-user . "frenzieddoll@gmail.com")
+  			 (mew-smtp-server . "smtp.gmail.com")
+  			 (mew-fcc . "%Sent")
+  			 (mew-imap-trash-folder . "%[Gmail]/Trash")
+  			 (mew-use-cached-passwd . t)
+  			 (mew-ssl-verify-level . 0)
+
+  			 (mew-use-cached-passwd . t)
+  			 (mew-use-master-passwd . t)
+  			 ;; 未読メールにUマークを付ける
+  			 (mew-use-unread-mark . t)
+
+			 (mew-use-text/html . t)
+			 (browse-url-broser-function . eww-browser-url)
+			 (mew-thread-indent-strings . ["+" "+" "|" " "])
+			 )
+	:config
+	(define-mail-user-agent
+	  'mew-user-agent
+	  'mew-user-agent-compose
+	  'mew-draft-send-message
+	  'mew-draft-kill
+	  'mew-send-hook)
+	;; (setq mew-thread-indent-strings  ["+" "+" "|" " "])
+	(leaf *addSettings
+	  :when (fboundp 'shr-render-region)
+	  :when (fboundp 'libxml-parse-html-region)
+	  :custom ((mew-prog-text/html . shr-render-region))
+	  )
+	)
 
 
 (leaf *lsp-tools
