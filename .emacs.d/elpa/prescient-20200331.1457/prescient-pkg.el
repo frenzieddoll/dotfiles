20c4f35cbe8d1746a16198efdf25af38c8c77c3a
frenzieddoll <frenzieddoll@gmail.com>
2020-04-02 04:10:47 +0900

    20200401 パッケージ更新

---
+++ .emacs.d/elpa/prescient-20200331.1457/prescient-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/toshiaki/.emacs.d/elpa/prescient-20200331.1457/prescient.el  -*- no-byte-compile: t -*-
+(define-package "prescient" "20200331.1457" "Better sorting and filtering" '((emacs "25.1")) :commit "aa3110281a0622fa7b5da115cefb71a319b5b1a0" :keywords '("extensions") :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :url "https://github.com/raxod502/prescient.el")
