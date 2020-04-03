e04e35f7c697f541f1a7fab778d9c40433826e69
frenzieddoll <frenzieddoll@gmail.com>
2020-04-04 02:13:35 +0900

    20200404 mpv-rifle.sh の更新

---
+++ .emacs.d/elpa/highlight-indent-guides-20200401.1707/highlight-indent-guides-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/toshiaki/.emacs.d/elpa/highlight-indent-guides-20200401.1707/highlight-indent-guides.el  -*- no-byte-compile: t -*-
+(define-package "highlight-indent-guides" "20200401.1707" "Minor mode to highlight indentation" '((emacs "24")) :commit "c2c9de4d01edfb89609c91d4d7f1d81312015a2c" :authors '(("DarthFennec" . "darthfennec@derpymail.org")) :maintainer '("DarthFennec" . "darthfennec@derpymail.org") :url "https://github.com/DarthFennec/highlight-indent-guides")
