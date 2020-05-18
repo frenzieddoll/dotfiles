32071e2b99077435263d8b2dce0be587245fba74
frenzieddoll <frenzieddoll@gmail.com>
2020-05-18 15:20:39 +0900

    20200518 パッケージ更新

--- .emacs.d/elpa/leaf-20200422.644/leaf-pkg.el
+++
@@ -1,3 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-;;; Generated package description from /home/toshiaki/.emacs.d/elpa/leaf-20200422.644/leaf.el  -*- no-byte-compile: t -*-
-(define-package "leaf" "20200422.644" "Simplify your init.el configuration, extended use-package" '((emacs "24.4")) :commit "27f21ed46a907a911222a72a31b41e414c956cfb" :keywords '("lisp" "settings") :authors '(("Naoya Yamashita" . "conao3@gmail.com")) :maintainer '("Naoya Yamashita" . "conao3@gmail.com") :url "https://github.com/conao3/leaf.el")
