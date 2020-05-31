c4668e57d8bf7aaca76e5728093b4c3d013f2091
frenzieddoll <frenzieddoll@gmail.com>
2020-05-31 19:37:01 +0900

    20200531 パッケージ更新

---
+++ .emacs.d/elpa/helm-20200531.619/helm-pkg.el
@@ -0,0 +1,9 @@
+(define-package "helm" "20200531.619" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.6.2"))
+  :commit "6500dc4e6b433f90844e6008fd992c1d96a17548" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/helm-core-20200529.557/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20200529.557" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "6500dc4e6b433f90844e6008fd992c1d96a17548" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
