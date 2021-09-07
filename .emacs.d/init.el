72eb13df74b8b07597dfafa2188f1426ae42da26
frenzieddoll <frenzieddoll@gmail.com>
2021-09-08 02:59:16 +0900

    change el-get for lsp-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1734,7 +1734,7 @@
 (leaf lsp
   :tag "out-of-MELPA"
   :added "2021-09-05"
-  :el-get {{user}}/lsp
+  :el-get emacs-lsp/lsp-mode
   :require t
   :unless (string-match "Raspberrypi" (system-name))
   :hook ((haskell-mode-hook . lsp)
