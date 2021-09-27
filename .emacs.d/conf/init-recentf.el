e5c0df89aeb54c37a2275961be9fe4d079673906
H,Toshiaki <frenziddoll@gmail.com>
2021-09-28 01:20:03 +0900

    キャッシュ削除

--- .emacs.d/conf/init-recentf.el
+++
@@ -1,6 +0,0 @@
-(defvar recentf-max-saved-items 2000)
-(defvar recentf-auto-cleanup 'never)
-(defvar recentf-exclude '("/recentf" "COMMIT_EDITMSG" "/.?TAGS" "^/sudo:" "/\\.emacs\\.d/games/*-scores" "/\\.emacs\\.d/\\.cask/"))
-(setq recent-auto-save-timer (run-with-idle-timer 30 t 'recent-save-list))
-
-(recentf-mode 1)
