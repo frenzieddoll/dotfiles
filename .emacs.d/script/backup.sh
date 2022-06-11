5fb9d177e1741843656a083a8d429715dc8dd624
frenzieddoll <frenzieddoll@gmail.com>
2022-06-12 03:40:58 +0900

    fix init.el, backup.sh

--- .emacs.d/script/backup.sh
+++ .emacs.d/script/backup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+cat <<EOF > exclude-file
+Downloads
+Documents/programing
+lost+found
+SteamLibrary
+EOF
+
 BASEDIR="/mnt/HDDforBackup/" #バックアップ先の親ディレクトリ
 LATESTBKUP=$(ls $BASEDIR | grep backup- | tail -n 1) #直近のバックアップのディレクトリ名
 
-rsync -avh --link-dest="$BASEDIR/$LATESTBKUP" /mnt/HDDforData/ "/mnt/HDDforBackup/backup-$(date +%Y%m%d-%H%M%S)"
+rsync -avh $1 --exclude-from='exclude-file' --link-dest="$BASEDIR/$LATESTBKUP" /mnt/HDDforData/ "/mnt/HDDforBackup/backup-$(date +%Y%m%d-%H%M%S)"
