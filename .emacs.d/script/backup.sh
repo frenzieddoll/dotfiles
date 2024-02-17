2ec0cd7e675dfca423534d102bab24955be94440
frenzieddoll <frenzieddoll@gmail.com>
2024-02-18 02:11:51 +0900

    fix backup script

--- .emacs.d/script/backup.sh
+++ .emacs.d/script/backup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-excludeFile () {
-    cat <<EOF
-
-lost+found
-SteamLibrary
-.Trash-1000
-
-EOF
+# import
+checkExist () {
+    ls $1 > /dev/null 2>&1
 }
 
+error () {
+    echo $1
+    exit 1
+}
+
+# option analyze
 POSITIONAL_ARGS=()
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -18,6 +19,7 @@ while [[ $# -gt 0 ]]; do
         --run) optFlg="run"; shift;;
         -e|--external) extFlg=$2; shift; shift;;
         -s|--source) srcFlg=$2; shift; shift;;
+        --ssh) sshFlg=$2; shift; shift;;
         --) shift;  POSITIONAL_ARGS+=("$@"); set --;;
         -*) echo "[ERROR] Unknown option $1"; exit 1;;
         *)  POSITIONAL_ARGS+=("$1"); shift;;
@@ -25,6 +27,8 @@ while [[ $# -gt 0 ]]; do
 done
 set -- "POSITIONAL_ARGS[@]"  #// set $1, $2, ...
 unset POSITIONAL_ARGS
+
+# define variable
 
 #バックアップディレクトリ名
 date=$(date +%Y%m%d-%H%M%S)
@@ -35,15 +39,33 @@ OPT=$(case $optFlg in
           *) echo "-${optFlg//-/}";;
       esac)
 
+OPTSSH=$(case $sshFlg in
+             "") echo "";;
+             *) echo "-e ssh";;
+         esac)
+
 #バックアップ先の親ディレクトリ
 SNAP=$(case $srcFlg in
            "") echo "/mnt/HDDforBackup/";;
            *) echo $srcFlg;;
        esac)
 
-SRC="/mnt/HDDforData/"
+backupDir=$(case $sshFlg in
+                "") echo ${SNAP}backup-${date};;
+                *) echo ${sshFlg}:${SNAP}backup-${date};;
+            esac)
+
+SRC=$(case $srcFlg in
+          "") echo "/mnt/HDDforData/";;
+          *) echo $srcFlg;;
+      esac)
+
+
 LINK="--link-dest=../backup-last"
 
+# define function
+
+#
 excludeFile () {
     local configFileDefault="./backup.config"
     case $extFlg in
@@ -59,32 +81,36 @@ EOF
     esac
 }
 
-checkExist () {
-    ls $1 > /dev/null 2>&1
-}
-
-error () {
-    echo $1
-    exit 1
-}
-
 # バックアップ
 backup () {
-    rsync $OPT -e ssh --exclude-from=<(excludeFile) $LINK $SRC archlinuxrpi:${SNAP}backup-${date}
+    rsync $OPT $OPTSSH --exclude-from=<(excludeFile) $LINK $SRC $backupDir || \
+        error "faild backup"
 }
 
 # 最新のバックアップのリンク
 makeLink () {
     case $optFlg in
-        "run") ssh archlinuxrpi "cd /mnt/HDDforBackup; rm backup-last; ln -s ./backup-${date} ./backup-last" && \
-                     echo "make link" || error "cannot make link";;
+        # "run") ssh archlinuxrpi "cd /mnt/HDDforBackup; rm backup-last; ln -s ./backup-${date} ./backup-last" && \
+        #              echo "make link" || error "cannot make link";;
+        "run") funcLink && echo "make link" || error "cannot make link";;
         *) echo "dont make link";;
     esac
 }
 
-main () {
-    backup && makeLink || error "error"
+funcLink () {
+    local linkExpr="cd ${SNAP}; rm backup-last; ln -s ./backup-${date} ./backup-last"
+    case $sshFlg in
+        "") eval "$linkExpr";;
+        *) ssh $sshFlg "$linkExpr"
+    esac
 }
 
+main () {
+    backup && makeLink || error "faild main"
+}
+
+# funcLink
+
+# run
 # exit
 main
